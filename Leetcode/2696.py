fb5a492aa5e3ee7e0b42207eb1fc38e49cffb177
RJ <rj@Eureka.local>
2024-10-21 17:29:00 +0530

    stack_easy_03

---
+++ Leetcode/2696.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minLength(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        while "AB" in s or "CD" in s:
+            s = s.replace("AB", '')
+            s = s.replace("CD", '')
+            print (s)
+        return len(s)

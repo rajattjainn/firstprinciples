2f3c4c5242bacfc96123dd32c766c8399e8ba09c
RJ <rj@Eureka.local>
2024-10-27 15:11:47 +0530

    stack_easy_07

---
+++ Leetcode/1598.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minOperations(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: int
+        """
+        stack = []
+        for log in logs:
+            if log == "../":
+                if stack: stack.pop()
+            elif log != './':
+                stack.append(log)
+        
+        return (len(stack))
